c971f94f308ae187d41824f34ed1f676521ec81a
brunojss <bruno.jsscruz@gmail.com>
2025-08-16 17:22:15 -0300

    Criando métodos para o objeto

--- objetos/aula.js
+++ objetos/aula.js
@@ -6,14 +6,15 @@ Objeto => Coleção dinâmica de chave e valor
 const bruno = {
     nome: "Bruno J. Cruz",
     idade: 26,
+    descrever: function () {
+        console.log(`Meu nome é ${this.nome} e minha idade é ${this.idade} anos`)
+    }
 }
 
 console.log(bruno.nome)
 console.log(bruno.idade)
 console.log(bruno)
 
-bruno.altura = 1.84
-console.log(bruno)
+bruno.descrever();
 
-delete bruno.nome
-console.log(bruno)
+
