cde7bd480f5ed8465336aa771758591f8e322983
brunojss <bruno.jsscruz@gmail.com>
2025-08-16 20:46:13 -0300

    Adicionando e removendo itens de uma lista

---
+++ arrays/aula.js
@@ -0,0 +1,15 @@
+const alunos = ['João', 'Vitor', "Marina"];
+
+console.log(alunos[3])
+
+alunos.push('Renan');
+
+console.log(alunos[3])
+
+console.log("Ultimo item da lista: " + alunos.pop()) //retira da lista o ultimo elemento
+
+console.log(alunos)
+
+console.log("Primeiro item da lista: " + alunos.shift()) // retira o primeiro item da lista
+
+console.log(alunos)
