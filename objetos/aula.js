d99ad7a2e5243184543b95279380886f6d90d0e4
brunojss <bruno.jsscruz@gmail.com>
2025-08-16 17:18:07 -0300

    Manipulação de objetos

--- objetos/aula.js
+++ objetos/aula.js
@@ -1,5 +1,8 @@
 
+/*
+Objeto => Coleção dinâmica de chave e valor
 
+*/
 const bruno = {
     nome: "Bruno J. Cruz",
     idade: 26,
@@ -7,4 +10,10 @@ const bruno = {
 
 console.log(bruno.nome)
 console.log(bruno.idade)
+console.log(bruno)
+
+bruno.altura = 1.84
+console.log(bruno)
+
+delete bruno.nome
 console.log(bruno)
